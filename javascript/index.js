9d7c6fe995d868317323a2841a91d7bf880ce38a
Victor Turitzin <turitzinv@gmail.com>
2022-01-20 14:20:18 -0500

    Added click event to Workout List to show a table

--- javascript/index.js
+++ javascript/index.js
@@ -3,19 +3,20 @@
 /* Node Getters */
 const mainDiv = () => document.getElementById('main')
 const home = () => document.getElementById('home-page')
+const workoutList = () => document.getElementById('workout-list-page')
 
 /* Event Handlers, what happens when something triggers */
 const loadHomepage = () => {
-  const h1 = document.createElement('h1')
-  h1.className = 'center-align';
-  h1.innerText = 'Workout Schedule';
-  mainDiv().appendChild(h1)
-  //<h1 class="center-align">Workout Schedule</h1>
+  // const h1 = document.createElement('h1')
+  // h1.className = 'center-align';
+  // h1.innerText = 'Workout Schedule';
+  // mainDiv().appendChild(h1)
+  return ' <h1 class="center-align">Workout Schedule</h1> '
 }
 
 const workoutTemplate = () => {
   return `
-  <h1>Wokrout of the Week</h1>
+  <h1>Workout of the Week</h1>
     <table class="centered">
       <thead>
         <tr>
@@ -38,24 +39,39 @@ const workoutTemplate = () => {
     </table>
   `
 }
-
+//Below is click event for "Home" on the nav bar
 const homePageLink = () => {
   home().addEventListener('click', (e) => {
     e.preventDefault();
-    loadHomepage();
+    showHomepage();
+  })
+}
+//Below is click event for "Workout List" on the nav bar
+const workoutListLink = () => {
+  workoutList().addEventListener('click', (e) => {
+    e.preventDefault();
+    workout();
   })
 }
 
+
+
 /* Startup */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   //what happens when the page loads
-  loadHomepage(); //this will load 'Workout Schedule'
+  showHomepage(); //this will load 'Workout Schedule'
   homePageLink(); //when DOM loads, its going to render the click event that allows to render homepage
+  workoutListLink(); //this will load the table
 })
 
-const renderWorkoutTable = () => {
-  mainDiv().innerHTML = workoutTemplate()
+/* renders */
+const showHomepage = () => {
+  mainDiv().innerHTML = loadHomepage(); //adds in the h1 Workout Schedule
+}
+
+const workout = () => {
+  mainDiv().innerHTML = workoutTemplate() //adds in the table
 }
 
 
@@ -63,5 +79,6 @@ const renderWorkoutTable = () => {
 - created a DOMContentLoaded, adds in the title of 'Workout Schedule'
 when the page loads, adding in an h1 header.
 - Click Home and "Workout Schedule appears" on the main page
+- Click Workout List and "Work out of the week" will appear with a table
 
 */
